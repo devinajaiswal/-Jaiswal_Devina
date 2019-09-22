54c12118d9c9e8c914f60a0601058044e347f953
devina8 <jaiswal.de@husky.neu.edu>
2019-09-22 14:10:18 -0400

    First commit

---
+++ Lab01/src/Business/Product.java
@@ -0,0 +1,51 @@
+package Business;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author devinajaiswal
+ */
+public class Product {
+    private String name;
+    private String price;
+    private String availNum;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getAvailNum() {
+        return availNum;
+    }
+
+    public void setAvailNum(String availNum) {
+        this.availNum = availNum;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    private String description;
+    
+}
